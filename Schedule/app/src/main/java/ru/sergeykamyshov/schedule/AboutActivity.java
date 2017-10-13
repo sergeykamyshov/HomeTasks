a093123b6e0d965dbc305f5c666e2141e28ebb4f
sergeykamyshov <kamishovsergey@gmail.com>
2017-10-13 22:07:30 +0300

    Добавил экран "О приложении" и пункты меню, переходы которых ведут друг на друга

--- Schedule/app/src/main/java/ru/sergeykamyshov/schedule/AboutActivity.java
+++ Schedule/app/src/main/java/ru/sergeykamyshov/schedule/AboutActivity.java
@@ -2,34 +2,35 @@ package ru.sergeykamyshov.schedule;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ScheduleActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_schedule);
+        setContentView(R.layout.activity_about);
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
-            actionBar.setTitle(getString(R.string.action_bar_title_schedule));
+            actionBar.setTitle(getString(R.string.action_bar_title_about));
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(getString(R.string.menu_item_about));
+        menu.add(getString(R.string.menu_item_schedule));
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == 0) {
-            startActivity(new Intent(this, AboutActivity.class));
+            startActivity(new Intent(this, ScheduleActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
