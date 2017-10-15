29d8f5d540e284e0cecfdb811354ebfcb3d5dc16
sergeykamyshov <kamishovsergey@gmail.com>
2017-10-15 22:17:32 +0300

    Добавил поиск по списку станций

--- Schedule/app/src/main/java/ru/sergeykamyshov/schedule/StationListActivity.java
+++ Schedule/app/src/main/java/ru/sergeykamyshov/schedule/StationListActivity.java
@@ -6,6 +6,10 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -19,7 +23,7 @@ import ru.sergeykamyshov.schedule.models.City;
 
 import static ru.sergeykamyshov.schedule.loaders.StationLoader.STATION_LOADER_ID;
 
-public class StationListActivity extends BaseActivity implements LoaderManager.LoaderCallbacks<List<City>> {
+public class StationListActivity extends BaseActivity {
 
     private StationRecyclerAdapter mAdapter;
 
@@ -28,42 +32,70 @@ public class StationListActivity extends BaseActivity implements LoaderManager.L
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_station_list);
 
-        setActionBarTitle(getString(R.string.action_bar_title_stations));
+        // Подготавливаем Toolbar необоходим для размещения на нем поиска
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(getString(R.string.action_bar_title_stations));
 
-        // Настраиваем RecyclerView
+        // Подготавливаем RecyclerView
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.stations_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        // Создаем и устанавливаем адаптер для RecyclerView
         mAdapter = new StationRecyclerAdapter(this, new ArrayList<City>());
         recyclerView.setAdapter(mAdapter);
 
-        // Загружаем данные из базы (или json файла если первый запуск после обновления)
-        getSupportLoaderManager().initLoader(STATION_LOADER_ID, null, this);
+        // Инициируем работу Loader'a, который достанет нам данные из базы или json файла (если это первый запуск после обновления)
+        getSupportLoaderManager().initLoader(STATION_LOADER_ID, null, mLoaderCallbacks);
     }
 
     @Override
-    public Loader<List<City>> onCreateLoader(int id, Bundle args) {
-        return new StationLoader(this);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_stations_list, menu);
+
+        MenuItem search = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) search.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (mAdapter != null) {
+                    mAdapter.filterByQuery(newText);
+                }
+                return true;
+            }
+        });
+        return true;
     }
 
-    @Override
-    public void onLoadFinished(Loader<List<City>> loader, List<City> data) {
-        // Скрываем ProgressBar после выполнения loader'а
-        ProgressBar progressBar = (ProgressBar) findViewById(R.id.bar_progress);
-        progressBar.setVisibility(View.GONE);
-        if (data != null && !data.isEmpty()) {
-            mAdapter.setDataSet(data);
-            mAdapter.notifyDataSetChanged();
-        } else {
-            // Если список пуст, то необходимо вывести информацию об этом
-            TextView emptyList = (TextView) findViewById(R.id.text_empty_station_list);
-            emptyList.setVisibility(View.VISIBLE);
+    private LoaderManager.LoaderCallbacks<List<City>> mLoaderCallbacks = new LoaderManager.LoaderCallbacks<List<City>>() {
+        @Override
+        public Loader<List<City>> onCreateLoader(int id, Bundle args) {
+            return new StationLoader(getApplicationContext());
         }
-    }
 
-    @Override
-    public void onLoaderReset(Loader<List<City>> loader) {
-        mAdapter.setDataSet(new ArrayList<City>());
-        mAdapter.notifyDataSetChanged();
-    }
+        @Override
+        public void onLoadFinished(Loader<List<City>> loader, List<City> data) {
+            // Скрываем ProgressBar после выполнения возвращения результата от loader'а
+            ProgressBar progressBar = (ProgressBar) findViewById(R.id.bar_progress);
+            progressBar.setVisibility(View.GONE);
+            if (data != null && !data.isEmpty()) {
+                mAdapter.setDataSet(data);
+                mAdapter.notifyDataSetChanged();
+            } else {
+                // Если список пуст, то необходимо вывести информацию об этом
+                TextView emptyList = (TextView) findViewById(R.id.text_empty_station_list);
+                emptyList.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onLoaderReset(Loader<List<City>> loader) {
+            mAdapter.setDataSet(new ArrayList<City>());
+            mAdapter.notifyDataSetChanged();
+        }
+    };
 }
--- Schedule/app/src/main/java/ru/sergeykamyshov/schedule/adapters/StationRecyclerAdapter.java
+++ Schedule/app/src/main/java/ru/sergeykamyshov/schedule/adapters/StationRecyclerAdapter.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ru.sergeykamyshov.schedule.R;
@@ -29,11 +30,18 @@ import static ru.sergeykamyshov.schedule.ScheduleActivity.REQUEST_PARAM_STATION_
 public class StationRecyclerAdapter extends RecyclerView.Adapter<StationRecyclerAdapter.ViewHolder> {
 
     private Context mContext;
+    /**
+     * Список данных используемый адаптером для отображения списка
+     */
     private List<City> mDataSet;
+    /**
+     * Всегда полный список данных используемый для поиска данных по запросу
+     */
+    private List<City> mFullDataSet;
 
     public StationRecyclerAdapter(Context context, List<City> dataSet) {
         mContext = context;
-        mDataSet = dataSet;
+        setDataSet(dataSet);
     }
 
     @Override
@@ -48,13 +56,15 @@ public class StationRecyclerAdapter extends RecyclerView.Adapter<StationRecycler
         // Формирование заголовка категории в формате: "Страна, Город"
         final String countryCityTitle = city.getCountryTitle() + ", " + city.getCityTitle();
         holder.mCountryCityTitle.setText(countryCityTitle);
-        // Удаляем все станции для предыдущей категории, чтобы они не накапливались
+        // Удаляем все станции для предыдущей категории, чтобы не накапливались
         holder.mStationContainerLayout.removeAllViews();
         for (final Station station : city.getStations()) {
+            // Т.к. у городов разное кол-вол станций, то создаем и заполняем каждый список станций программно в цикле
             View stationLayout = LayoutInflater.from(mContext).inflate(R.layout.station_list_item, holder.mStationContainerLayout, false);
             final TextView stationTitle = stationLayout.findViewById(R.id.stationTitleTextView);
             stationTitle.setText(station.getStationTitle());
 
+            // Нажатие на иконку "Подробнее" должно открывать новый экран с полной информацией
             ImageView moreInfoImage = stationLayout.findViewById(R.id.stationMoreInfoImageView);
             moreInfoImage.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -68,6 +78,7 @@ public class StationRecyclerAdapter extends RecyclerView.Adapter<StationRecycler
                 }
             });
 
+            // Нажатие на конкретную станцию должно заполнять поле на экране "Расписание" текущим значением
             stationLayout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -84,6 +95,59 @@ public class StationRecyclerAdapter extends RecyclerView.Adapter<StationRecycler
         }
     }
 
+    /**
+     * Производит фильтрацию данных адаптера по переданому запросу
+     *
+     * @param query - запрос поиска
+     */
+    public void filterByQuery(String query) {
+        if (query.isEmpty()) {
+            // Если пользователь полностью очистил запрос в поиске, то показать полный список
+            mDataSet = mFullDataSet;
+        } else {
+            List<City> filteredCities = new ArrayList<>();
+            // Поиск всегда осуществляется по полному списку
+            for (City city : mFullDataSet) {
+                // Искать необходимо с игнорированием регистра, поэтому приводим все к нижнему регистру
+                String lowerCaseQuery = query.toLowerCase();
+                // Возможен поиск по городу или стране, мало ли
+                if (city.getCountryTitle().toLowerCase().contains(lowerCaseQuery) || city.getCityTitle().toLowerCase().contains(lowerCaseQuery)) {
+                    // Если есть совпадение по городу/стране, то сразу добавляем весь город в список
+                    filteredCities.add(city);
+                    continue;
+                }
+                List<Station> filteredStations = new ArrayList<>();
+                // Если по городу/стране не нашли, надо искать по отдельным станциям
+                for (Station station : city.getStations()) {
+                    if (station.getStationTitle().toLowerCase().contains(lowerCaseQuery)) {
+                        // Если станция подошла, то добавляем ее в новый список - отсортированных станций
+                        filteredStations.add(station);
+                    }
+                }
+                // Список должен быть заполнен, если нашли подходящие станции
+                if (!filteredStations.isEmpty()) {
+                    // Создаем новый город со списком только подходящих станций
+                    City cityWithFilteredStations = new City(city.getCountryTitle(), city.getCityTitle(), filteredStations);
+                    filteredCities.add(cityWithFilteredStations);
+                }
+            }
+            // Устанавливаем адаптера новый список данных для отображения
+            mDataSet = filteredCities;
+        }
+        notifyDataSetChanged();
+    }
+
+    /**
+     * Помещает данные в два списка, т.к. один из них всегда должен содержать полную информацию (mFullDataSet),
+     * а другой отфильтрованные данные (mDataSet)
+     *
+     * @param dataSet - список с данными
+     */
+    public void setDataSet(List<City> dataSet) {
+        mFullDataSet = dataSet;
+        mDataSet = dataSet;
+    }
+
     @Override
     public int getItemCount() {
         return mDataSet.size();
@@ -98,9 +162,5 @@ public class StationRecyclerAdapter extends RecyclerView.Adapter<StationRecycler
             mCountryCityTitle = itemView.findViewById(R.id.countryCityTitleTextView);
             mStationContainerLayout = itemView.findViewById(R.id.stationsContainerLayout);
         }
-    }
-
-    public void setDataSet(List<City> dataSet) {
-        mDataSet = dataSet;
     }
 }
