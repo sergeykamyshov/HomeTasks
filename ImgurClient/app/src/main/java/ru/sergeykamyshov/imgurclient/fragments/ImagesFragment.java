3c1fb98530c8f2e019efdf12548f14e131ff99b9
sergeykamyshov <kamishovsergey@gmail.com>
2017-11-22 22:14:54 +0300

    Добавил в проект RecyclerView

--- ImgurClient/app/src/main/java/ru/sergeykamyshov/imgurclient/fragments/ImagesFragment.java
+++ ImgurClient/app/src/main/java/ru/sergeykamyshov/imgurclient/fragments/ImagesFragment.java
@@ -3,15 +3,26 @@ package ru.sergeykamyshov.imgurclient.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import ru.sergeykamyshov.imgurclient.R;
+
 public class ImagesFragment extends Fragment {
+
+    public static final int SPAN_COUNT = 2;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_images, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_images);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(SPAN_COUNT, StaggeredGridLayoutManager.VERTICAL));
+
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
